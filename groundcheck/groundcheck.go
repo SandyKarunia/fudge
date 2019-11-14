d89da04bfb95936f7d7155c523d80cdbce0315ef
Sandy Karunia <sandykarunia94@gmail.com>
2019-11-14 22:28:38 +0800

    rollback, remove solvers

--- groundcheck/groundcheck.go
+++ groundcheck/groundcheck.go
@@ -2,7 +2,6 @@ package groundcheck
 
 import (
 	"errors"
-	"github.com/fatih/color"
 	"github.com/sandykarunia/fudge/groundcheck/checkers"
 )
 
@@ -22,45 +21,21 @@ type groundCheckImpl struct {
 	c checkers.Checkers
 }
 
-type checkerAndSolver struct {
-	checkerFunc func() bool
-	solverFunc  func()
-}
-
 func (g *groundCheckImpl) CheckAll() error {
 	var errRes error
 
-	var checkerFuncs = []checkerAndSolver{
-		{g.c.CheckSudo, nil},
-		{g.c.CheckLibcapDevPkg, nil},
-		{g.c.CheckIsolateBinaryExists, nil},
-		{g.c.CheckIsolateBinaryExecutable, nil},
+	var checkerFuncs = []func() bool{
+		g.c.CheckSudo,
+		g.c.CheckLibcapDevPkg,
+		g.c.CheckIsolateBinaryExists,
+		g.c.CheckIsolateBinaryExecutable,
 	}
 
 	// we don't want to interrupt the checks (i.e. put return inside the loop)
 	// because we want the loop to keep going, as the functions will provide nice messages
 
-	for _, cns := range checkerFuncs {
-		// try to check
-		ok := cns.checkerFunc()
-		if ok {
-			continue
-		}
-
-		// if there is no solver function, then error and just continue
-		if cns.solverFunc == nil {
-			errRes = errCheckAllFailed
-			continue
-		}
-
-		color.HiMagenta("Trying to solve the problem...")
-
-		// try to solve
-		cns.solverFunc()
-
-		// check again
-		ok = cns.checkerFunc()
-		if !ok {
+	for _, fn := range checkerFuncs {
+		if !fn() {
 			errRes = errCheckAllFailed
 		}
 	}
